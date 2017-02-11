c1016cf1c8b523f6626c3962debd883ee6f83a68
Dan Buda <danbuda@gmail.com>
2017-02-10 21:44:07 -0600

    a few new component files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import Header from './components/Header';
+import Albums from './components/Albums';
+import Footer from './components/Footer';
 
 const App = React.createClass({
   render() {
     return (
-      <h1>Hello</h1>
+    	<div>
+    		<Header />
+      		<Albums />
+      		<Footer />
+      	</div>
     );
   }
 });
-
 
 export default App;
---
+++ src/components/Albums.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default React.createClass({
+	render() {
+		return (
+			<h1>Albums go here</h1>
+		);
+	}
+});
